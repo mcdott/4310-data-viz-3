6c417c34bca0824abc6f148b4d098c6a14793715
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-25 12:08:45 -0400

    Adds d3-tutorial React bar chart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BarChart from "./BarChart";
+// Import useState and useEffect
+import { useState, useEffect } from "react";
+import * as d3 from "d3"; // import D3
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    d3.csv("./cities.csv").then((data) => {
+      data.sort((a, b) => a.country < b.country);
+      setData(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <BarChart data={data} />
       </header>
     </div>
   );
