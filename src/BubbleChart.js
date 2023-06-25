734be78fe7d79a409bbef89e7944e617efe7060e
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-25 15:14:52 -0400

    Sets vertical axis to percent tracked, horizontal axis to speed, bubble size to target size, and bubble colour to target colour

--- src/BubbleChart.js
+++ src/BubbleChart.js
@@ -4,18 +4,29 @@ import { useD3 } from "./useD3";
 function BubbleChart({ data }) {
   const ref = useD3(
     (svg) => {
-      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+      const margin = { top: 20, right: 30, bottom: 60, left: 60 };
       const width = 800 - margin.left - margin.right;
       const height = 500 - margin.top - margin.bottom;
 
+      // Define speed array to map numbers to category names
+      const speeds = [
+        "",
+        "very very slow",
+        "very slow",
+        "slow",
+        "medium speed",
+        "fast",
+      ];
+
       const xScale = d3
-        .scaleLinear()
-        .domain(d3.extent(data, (d) => d.speed))
-        .range([0, width]);
+        .scaleBand()
+        .domain(speeds)
+        .range([0, width])
+        .paddingInner(0.05);
 
       const yScale = d3
         .scaleLinear()
-        .domain(d3.extent(data, (d) => d.size))
+        .domain([0, 100]) // percent ranges from 0 to 100
         .range([height, 0]);
 
       const colorScale = d3
@@ -25,8 +36,8 @@ function BubbleChart({ data }) {
 
       const radius = d3
         .scaleSqrt()
-        .domain([0, d3.max(data, (d) => d.percent)])
-        .range([0, 50]);
+        .domain([1, 5]) // size ranges from 1 (xs) to 5 (xl)
+        .range([10, 50]);
 
       const g = svg
         .append("g")
@@ -37,18 +48,32 @@ function BubbleChart({ data }) {
         .enter()
         .append("circle")
         .attr("class", "bubble")
-        .attr("cx", (d) => xScale(d.speed))
-        .attr("cy", (d) => yScale(d.size))
-        .attr("r", (d) => radius(d.percent))
+        .attr("cx", (d) => xScale(speeds[d.speed]) + xScale.bandwidth() / 2) // center the bubbles
+        .attr("cy", (d) => yScale(d.percent))
+        .attr("r", (d) => radius(d.size))
         .style("fill", (d) => colorScale(d.color));
 
       // Add the X Axis
       g.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(xScale))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("y", 40)
+        .attr("x", width / 2)
+        .attr("text-anchor", "middle")
+        .text("Speed");
 
       // Add the Y Axis
-      g.append("g").call(d3.axisLeft(yScale));
+      g.append("g")
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -50)
+        .attr("x", -(height / 2))
+        .attr("text-anchor", "middle")
+        .text("Percent tracked");
     },
     [JSON.stringify(data)]
   );
@@ -62,7 +87,11 @@ function BubbleChart({ data }) {
         marginRight: "0px",
         marginLeft: "0px",
       }}
-    ></svg>
+    >
+      <g className='plot-area' />
+      <g className='x-axis' />
+      <g className='y-axis' />
+    </svg>
   );
 }
 
